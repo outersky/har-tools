3da00c09a8601a5add1cdf79c2452d19be6a5d4d
Samir Bhatt <sbbhatt8@gmail.com>
2014-10-27 06:00:26 -0700

    Fixed Usage message to accurately reflect option switches

--- harx.go
+++ harx.go
@@ -146,10 +146,10 @@ func main() {
 		fmt.Println(`
 usage: harx [options] har-file
     -l                  List files , lead by [index]
-    -lp urlPattern      like -l , but filter with urlPattern
+    -lu urlPattern      like -l , but filter with urlPattern
     -a dir              extract All content to [dir]
     -i Index            extract the [index] content , need run with -l first to get [index]
-    -p urlPattern dir   like -a , but filter with urlPattern
+    -u urlPattern dir   like -a , but filter with urlPattern
 
         `)
 		return
